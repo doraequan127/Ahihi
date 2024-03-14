4bce87b9a5cf4601f7ac968a8872a6afb2a3e4b8
Quân Superdora <doraequan127@gmail.com>
2024-03-14 17:44:51 +0700

    a

--- Assets/Ahihi/ahihi.cs
+++ Assets/Ahihi/ahihi.cs
@@ -92,21 +92,12 @@ public class ahihi : MonoBehaviour
 
     private void Awake()
     {
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        print(linhtinh.oe());
     }
 
     private void Start()
     {
-        //GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x,
-        //    GetComponent<RectTransform>().sizeDelta.x * Screen.height / Screen.width);
-
-        foreach (var i in WebCamTexture.devices)
-            if (i.isFrontFacing)
-                webCamTexture = new WebCamTexture(i.name);
-
-        //webCamTexture = new WebCamTexture();
-        GetComponent<RawImage>().texture = webCamTexture;
-        webCamTexture.Play();
+        
     }
 
     private void Update()
---
+++ Assets/Ahihi/linhtinh.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linhtinh : MonoBehaviour
+{
+    public static string vlahovic = "dusan vlahovic";
+    public static int oe()
+    {
+        return 3 + 5;
+    }
+}
